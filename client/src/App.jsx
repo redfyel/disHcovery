fd6688ab74a504bb9b60f83bbdbec005d7075cdc
redfyel <lianis.ee7@gmail.com>
2025-02-12 22:26:49 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header/Header'
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World!</h1>
+      <h1>Hello Team 4</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header(){
+    return (
+        <div>
+            <h1 id = "header-h1">Hello Header</h1>
+        </div>
+    )
+}
+
+export default Header;
