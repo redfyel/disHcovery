508f6c06b40428334db0e9981d01609c0c0d24a9
redfyel <lianis.ee7@gmail.com>
2025-02-14 21:06:01 +0530

    Initial:created components

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css'
 function Header(){
     return (
         <div>
-            <h1 id = "header-h1">Hello Header</h1>
+            <h1 id = "header-h1">Hello Header, Laasya</h1>
         </div>
     )
 }
