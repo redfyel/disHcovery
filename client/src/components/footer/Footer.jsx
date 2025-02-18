f4e13cb658d7cc09298c4ea99bdf7363669417b0
redfyel <lianis.ee7@gmail.com>
2025-02-18 22:47:22 +0530

    Added Root layout, Routing error files and desgined App.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <div>
+          <h1 id = "header-h1">Hello Footer</h1>
+        </div>
+    </div>
   )
 }
 
