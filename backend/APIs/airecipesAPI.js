2e59e03ce854e9dc0c01a78d56a909516df3e780
redyel <lianis.ee7@gmail.com>
2026-02-07 00:26:29 +0530

    model migration

--- backend/APIs/airecipesAPI.js
+++ backend/APIs/airecipesAPI.js
@@ -163,7 +163,7 @@ airecipesApp.post("/generate-recipe-from-image", async (req, res) => {
 `;
 
     
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     const parts = [
       {text: prompt},
       {
